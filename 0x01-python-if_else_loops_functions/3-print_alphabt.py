6a8f7daa3424215de86682dca86ca5a31e67b851
NewGit1999 <yasminebenali32@gmail.com>
2023-03-07 08:27:44 -0800

    no e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for L in range(97, 123):
-    if chr(L) is not 'q' and chr(L) is not 'e':
-        print("{}".format(chr(L)), end="")
+for let in range(97, 123):
+    if chr(let) is not 'q' and chr(let) is not 'e':
+        print("{}".format(chr(let)), end="")
