8cbbc673d415f7b88f98cb5ab57921a402005d60
NewGit1999 <yasminebenali32@gmail.com>
2023-03-10 08:44:35 -0800

    remove c and C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for character in range (len(my_string)):
+        if my_string[character] != 'c' and my_string[character] != 'C':
+            new_string += my_string[character]
+    return new_string
