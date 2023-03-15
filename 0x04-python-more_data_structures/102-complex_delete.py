47d43b2001d831011bfc6aa3effe4fabda27533b
NewGit1999 <yasminebenali32@gmail.com>
2023-03-15 13:03:41 -0700

    delete keys specific

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     for ky in list(a_dictionary.keys()):
-        del a_dictionary[ky]
+        if a_dictionary[ky] is value:
+            del a_dictionary[ky]
     return a_dictionary
