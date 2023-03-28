bf668de6c16a7f3b73b7d8216f46a52aa2dcd545
NewGit1999 <yasminebenali32@gmail.com>
2023-03-28 09:01:13 -0700

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""defines a class square"""
+
+
+class Square:
+    """define a square"""
+
+    def __init__(self, size):
+        """present the instance attribute."""
+
+        self.__size = size
