21854fbf9b2ed95cb4d03d437405155f63385d4e
NewGit1999 <yasminebenali32@gmail.com>
2023-04-11 08:32:06 -0700

    my integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,8 +7,8 @@ class MyInt:
 
     def __eq__(self, func):
         """opposite of what default implementation returnd"""
-        return super().self__ne__(func)
+        return super().__ne__(func)
 
     def __ne__(self, func):
         """opposite of defualt implementation"""
-        return super().self__eq__(func)
+        return super().__eq__(func)
