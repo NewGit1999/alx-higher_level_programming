180fb0518bc9fbea4c749df4c28c999a373efcb7
NewGit1999 <yasminebenali32@gmail.com>
2023-04-11 05:54:30 -0700

    sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""my list task"""
+
+
+class MyList:
+    """inherit from list"""
+
+    def print_sorted(self):
+        """print sorted list"""
+
+        print(sorted(self))
