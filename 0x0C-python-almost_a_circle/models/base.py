a635370d6773f439dc3c47cc94b91aefd5730af9
NewGit1999 <yasminebenali32@gmail.com>
2023-04-17 10:48:27 -0700

    updates to base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,6 +8,7 @@ import csv
 
 class Base:
     """present base model"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -23,7 +24,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """return json string representation of list dict"""
         if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
+            return ("[]")
         return (json.dumps(list_dictionaries))
 
     @classmethod
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""rectangle module"""
+
+from models.base import Base
+import sys
+
+
+class Rectangle(Base):
+    """rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialize attributes"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """retrieve width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """retrieve x"""
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """set x"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """retrieve y"""
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """set y"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """returns the area value of rectangle"""
+        return (self.__height * self.__width)
+
+    def display(self):
+        """prints in stdout rectangle with character #"""
+        print('\n' * self.y, end="")
+        for a in range(self.height):
+            print('' * self.x, end="")
+            print('#' * self.width)
+
+    def __str__(self):
+        """return string about rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, 
+                                                       self.width, self.height)
