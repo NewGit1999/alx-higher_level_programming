8fb38fbc8e24b45d572a2e173cdc538e4ece7e2d
NewGit1999 <yasminebenali32@gmail.com>
2023-06-13 15:49:25 +0000

    computes factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function factorial (a) {
+  if (isNaN(a) | a === 1) {
+    return (1);
+  } else {
+    return (a * factorial(a - 1));
+  }
+}
+console.log(factorial(process.argv[2]));
